8bf5d82812c96435662b7bdcc6b8b9f1446f946c
John-Gell <jgell4@gmail.com>
2020-05-21 08:07:45 -0400

    git woes again

--- P02-Three_Pillars/Three_Pillars.js
+++ P02-Three_Pillars/Three_Pillars.js
@@ -1,32 +1,15 @@
-class Bookshelf{
-    constructor() {
-        this.favoriteBooks = [];
-    }
-    addFavoriteBook(bookName) {
-        if (bookName.includes("Great") || bookName.includes("great")){
-          /*skips adding the book if the title contains the word 'great'*/
-        }
-        else {
-        //   bookName.push(bookName)
-        this.favoriteBooks.push(bookName);
-        }
-      }
-    printFavoriteBooks(){
-        let listNo=1;
-    console.log('The books for the ages:')
-        for(let favoriteBook of Bookshelf1.favoriteBooks) {
-            console.log(String(listNo)+". "+favoriteBook);
-            listNo++;
-        }
-    }
-      
+function fakeAjax(url,cb) {
+	setTimeout(function fakeLoadingDelay(){
+		cb([
+			"A Song of Ice and Fire",
+			"The Great Gatsby",
+			"Crime & Punishment",
+			"Great Expectations",
+			"You Don't Know JS"
+        ]);
+        console.log(cb);
+	},500);
 }
-let Bookshelf1 = new Bookshelf;
 
-Bookshelf1.addFavoriteBook('The Subpar Gatsby');
-Bookshelf1.addFavoriteBook('The Great gatsby');
-Bookshelf1.addFavoriteBook('The Windup Girl');
-Bookshelf1.addFavoriteBook('The Great Gusty Gunter')
-Bookshelf1.addFavoriteBook("Harry Potter and the Sorcerer's Stone")
-
-Bookshelf1.printFavoriteBooks();
+let BOOK_API = "fake.url.com";
+let newBooks = [];
